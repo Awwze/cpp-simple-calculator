8168d45c9d9cc96014ab54ccdff4e20cc41e1c74
PonIdk <thefloopa@gmail.com>
2024-11-14 21:30:07 +0500

    Done version

--- main.cpp
+++ main.cpp
@@ -2,4 +2,31 @@
 
 int main(){
 
+    //a and b variables for nums
+    float a, b, res;
+
+    //variable for action
+    char action;
+
+    //output
+    std::cout << "Welcome to my calculator! " << std::endl;
+    std::cout << "Enter first number: " << std::endl;
+    std::cin >> a;
+    std::cout << "Nice, now enter second number: " << std::endl;
+    std::cin >> b;
+    std::cout << "Now choose the action (+,-,*,/): ";
+    std::cin >> action;
+
+    //action checker with "if"
+    if (action == '+')
+        res = a + b;
+    else if (action == '-')
+        res = a - b;
+    else if (action == '*')
+        res = a * b;
+    else if (action == '/')
+        res = a / b;
+
+    //result output
+    std::cout << "Your answer is: " << res << std::endl;
 }
